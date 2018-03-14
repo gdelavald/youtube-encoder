a347394a88a8b8e02b4eb7824d09550024885b3f
Diego Sampaio <chinello@gmail.com>
2018-03-14 18:43:07 -0300

    Add Dockerfile

--- encode.js
+++ encode.js
@@ -2,7 +2,7 @@ const { Server } = require('ws');
 const ffmpeg = require('fluent-ffmpeg')
 const wsStream = require('websocket-stream')
 
-const server = new Server({port: 8000, perMessageDeflate: false});
+const server = new Server({port: process.env.PORT || 8000, perMessageDeflate: false});
 
 server.on('connection', function(websocket, req) {
 	const name = req.url.replace('/', '');
@@ -15,17 +15,21 @@ server.on('connection', function(websocket, req) {
 		.input(stream)
 		.videoCodec('libx264')
 		.audioCodec('libmp3lame')
-		.outputFPS(30)
-		.addOption('-ar', 44100)
-		.addOption('-g', 60)
-		.addOption('-crf', 18)
-		.addOption('-pix_fmt', 'yuv420p')
-		.addOption('-threads', 6)
-		.addOption('-q:v', 3)
-		.addOption('-movflags', 'flagstart')
-		.addOption('-b:a', '384k')
-		.addOption('-maxrate', '750k')
-		.addOption('-bufsize', '3000k')
+		.outputFPS(24)
+		.preset('ultrafast')
+		.videoBitrate('400k')
+		.audioBitrate('128k')
+		.size('?x360')
+		// .addOption('-ar', 44100)
+		// .addOption('-g', 60)
+		// .addOption('-crf', 18)
+		// .addOption('-pix_fmt', 'yuv420p')
+		// .addOption('-threads', 6)
+		// .addOption('-q:v', 3)
+		// .addOption('-movflags', 'flagstart')
+		// .addOption('-b:a', '384k')
+		// .addOption('-maxrate', '750k')
+		// .addOption('-bufsize', '3000k')
 		.addOption('-f', 'flv')
 		.on('error', function(err) {
 			console.log(`Error: ${ err.message }`);
@@ -34,13 +38,14 @@ server.on('connection', function(websocket, req) {
 			console.log(`Convert complete${ stdout }`);
 		});
 
-  websocket.on('error', (err) => {
-    console.log('error', err)
-  });
+	websocket.on('error', (err) => {
+		console.log('error', err)
+	});
+
 	websocket.on('close', () => {
-    console.log(stream, 'close')
+		console.log(name, 'close')
 	});
 });
 
 
-console.log('Listening on port 8000');
+console.log(`Listening on port ${process.env.PORT || 8000}`);
